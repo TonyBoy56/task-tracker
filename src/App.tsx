190cd7cc0247e0fc4714863c77225d01d71a0265
Antonio Cruz <antonio.cruz5612@gmail.com>
2023-05-10 16:44:36 -0700

    remove unnecessary variables. change App method to => method, use React.FC library. Adda span.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-let  name: string;
-let age: number;
-let isStudent: boolean;
-let hobbies: string[];
-let role: [number, string];
-
-role = [5, "string"];
-
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      Hello World
+      <span className="heading">Taskify</span>
     </div>
   );
 }
