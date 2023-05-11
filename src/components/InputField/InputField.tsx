0028d162b68318a69d19f635ffe42e6734937448
Antonio Cruz <antonio.cruz5612@gmail.com>
2023-05-10 17:02:08 -0700

    add InputField component.

---
+++ src/components/InputField/InputField.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './InputField.css';
+
+interface InputFieldProps {}
+
+const InputField: FC<InputFieldProps> = () => (
+  <div className="InputField">
+    InputField Component
+  </div>
+);
+
+export default InputField;
