32a5e2bea3b0fd193ad0d7062308f2f5c2acc6a3
Antonio Cruz <antonio.cruz5612@gmail.com>
2023-05-15 13:43:02 -0700

    add a form with an input box and submit button.

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -4,9 +4,10 @@ import './InputField.css';
 interface InputFieldProps {}
 
 const InputField: FC<InputFieldProps> = () => (
-  <div className="InputField">
-    InputField Component
-  </div>
+  <form className='input'>
+    <input className='input_box' type="input" placeholder='Enter a task!' />
+    <button className='input_submit' type='submit'>Go</button>
+  </form>
 );
 
 export default InputField;
